3844fcb4d24e82ff5936c9459b6e0a397e543bb0
Pedroscsilva <pedroscsilva2001@gmail.com>
2023-05-08 15:58:25 -0300

    feat: adding a simple report method

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,25 @@
+from datetime import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(list):
+        all_fab_dates = [p["data_de_fabricacao"] for p in list]
+        old_date = min(all_fab_dates)
+
+        all_val_dates = [datetime.strptime(p["data_de_validade"], "%Y-%m-%d")
+                         for p in list]
+        now = datetime.now()
+        future_date = min(dt for dt in all_val_dates if dt > now).date()
+        counts = Counter(p["nome_da_empresa"] for p in list)
+        most_common = counts.most_common(1)[0][0]
+
+        return (
+            f"Data de fabricação mais antiga: {old_date}\n"
+            f"Data de validade mais próxima: {future_date}\n"
+            f"Empresa com mais produtos: {most_common}"
+        )
+        # f"""Data de fabricação mais antiga: {old_date}
+        # Data de validade mais próxima: {future_date}
+        # Empresa com mais produtos: {most_common}"""
